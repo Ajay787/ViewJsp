4c8cc12ec5148447ec2cf0ba9e423a9f5a7d03d3
ajaykumar <ajaykumar@192.168.43.63>
2018-04-15 14:58:18 +0530

    first commit

---
+++ JSPPro/src/main/java/com/app/basic/HelloController.java
@@ -0,0 +1,22 @@
+package com.app.basic;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "helloPage";
+    }
+    @RequestMapping("/first")
+    public String getAllList() {
+        return "First";
+    }
+}
